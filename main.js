73b1839138750dc623e47fc325f7603a77043230
Шелков Дмитрий <bobyorrr@gmail.com>
2021-02-15 18:05:48 +0300

    + ЖК «Любоград»

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const placemarkList = [
 	{
 		coord: [60.165021, 30.187849],
 		title: "ЖК «Новое Сертолово»",
-		linc: "<a href='Http://78.111.93.20:8557'>ЖК «Новое Сертолово»</a>",
+		linc: "<a href='http://78.111.93.20:8557'>ЖК «Новое Сертолово»</a>",
 	},
 	{
 		coord: [59.9578, 30.599826],
 		title: "ЖК «Ясно.Янино»",
-		linc: "<a href='Http://78.111.93.20:8557'>ЖК «Ясно.Янино»</a>",
+		linc: "<a href='http://78.111.93.20:8557'>ЖК «Ясно.Янино»</a>",
+	},
+	{
+		coord: [59.850693, 30.090244],
+		title: "ЖК «Любоград»",
+		linc: "<a href='http://192.168.6.60'>ЖК «Любоград»</a>",
 	},
 ];
 
@@ -16,8 +21,8 @@ ymaps.ready(function () {
 	var myMap = new ymaps.Map(
 		"mapApp",
 		{
-			center: [55.751574, 37.573856],
-			zoom: 9,
+			center: [59.97, 30.315635],
+			zoom: 10,
 		},
 		{
 			searchControlProvider: "yandex#search",
